576070cb07bdc70fb1878d62f7a2a980af4b2e0f
Sivan Mehta <Sivanmehta2@gmail.com>
2024-12-28 21:14:35 -0800

    placeholder

--- factory/UI.go
+++ factory/UI.go
@@ -1,42 +1,35 @@
 package factory
 
 import (
-	"fmt"
 	"main/tools"
-	"os"
-	"time"
 
 	"github.com/rivo/tview"
 )
 
 type UI struct {
-	View *tview.Modal
+	View *tview.Flex
 	App  *tview.Application
 }
 
-func currentTimeString() string {
-	t := time.Now()
-	return fmt.Sprintf(t.Format("Current time is 15:04:05"))
+func newText(text string) *tview.TextView {
+	return tview.NewTextView().
+		SetTextAlign(tview.AlignCenter).
+		SetText(text)
 }
 
 func GenerateUI(settings tools.SimulationSettings) *UI {
-	settingsString := tools.PrintArgs(settings)
-
 	app := tview.NewApplication()
-	view := tview.NewModal().
-		SetText(settingsString).
-		AddButtons([]string{"Quit"}).
-		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-			if buttonLabel == "Quit" {
-				app.Stop()
-				os.Exit(0)
-			}
-		})
 
-	app.SetRoot(view, false).Run()
+	flex := tview.NewFlex().
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("Left (1/2 x width of Top)"), 0, 1, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(tview.NewBox().SetBorder(true).SetTitle("Middle (3 x height of Top)"), 0, 3, false).
+			AddItem(tview.NewBox().SetBorder(true).SetTitle("Bottom (5 rows)"), 5, 1, false), 0, 2, false).
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("Right (20 cols)"), 20, 1, false)
+	app.SetRoot(flex, true).Run()
 
 	return &UI{
-		View: view,
+		View: flex,
 		App:  app,
 	}
 }
